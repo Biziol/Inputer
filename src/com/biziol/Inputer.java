a5df61781e16b6ba3cc59b4c5ee61660f99d1965
Biziol <88590615+Biziol@users.noreply.github.com>
2025-05-28 14:32:24 +0200

    set method clear console to non-static

--- src/com/biziol/Inputer.java
+++ src/com/biziol/Inputer.java
@@ -32,7 +32,7 @@ public class Inputer {
         }
     }
 
-    public static void clearConsole() {
+    public void clearConsole() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
     }
